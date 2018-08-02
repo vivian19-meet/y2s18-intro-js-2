caf750b3ecb56b427faea75bc78d7e6d598c300b
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 16:53:02 +0300

    done

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,17 @@
-// YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+// YOUR TASK: Add more pictures!"
+var pictures = ['./imgs/dog.jpg', "./imgs/waves.jpg","./imgs/pencils.jpg","./imgs/boats.jpg"];
 var currentIndex = 0;
-
+document.getElementsByTagName("img")[0].addEventListener("click", showNextPicture) 
 function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
+
+
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
+  document.getElementsByTagName("img")[0].src=pictures[currentIndex]
+
 
   // YOUR TASK: Finish this function!
 }
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,8 @@
 // Put all your JavaScript in this file!
+
+function getColor() {
+    alert("give me a color");
+    var c =prompt("choose a bg color")
+    var x =document.getElementsByTagName("body")
+    x[0].style.backgroundColor=c
+}
